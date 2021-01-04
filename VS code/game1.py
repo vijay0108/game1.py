9a9a8467855733d5199ed2e93eba1ecdb5752b33
Vijay Bajadoliya <56713404+vijay0108@users.noreply.github.com>
2021-01-04 14:17:24 +0530

    starting the game

--- VS code/game1.py
+++ VS code/game1.py
@@ -7,7 +7,7 @@ import time
 # Initial Steps to invite in the game:
 print("\nWelcome to Hangman game by vijay\n")
 name = input("Enter your name: ")
-print("Hello " + name + "! Best of Luck!")
+print("Hello " + name + "! Best of Luck! play well")
 time.sleep(2)
 print("The game is about to start!\n Let's play Hangman!")
 time.sleep(3)
